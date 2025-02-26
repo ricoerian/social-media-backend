ad079afd7c167e1b372a4adcf9b31f2de15ec419
ricoerian <richoeriansyahm@gmail.com>
2025-02-27 01:40:45 +0700

    database

--- config/database.go
+++ config/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
     // Ganti dengan DSN sesuai konfigurasi database kamu.
-    dsn := "root:1v1a5t312@tcp(127.0.0.1:3306)/sosmed?charset=utf8mb4&parseTime=True&loc=Local"
+    dsn := "root:yGkRgDaPonzbtJhrADzRiysVojCLLuTW@mysql.railway.internal/railway?charset=utf8mb4&parseTime=True&loc=Local"
     database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("Gagal terhubung ke database:", err)
